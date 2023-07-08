d78364db70431ac7eff624fcffe16635cb82ae57
Ceezyyyy <2417568802@qq.com>
2023-07-08 14:55:02 +0800

    mobile adaptation

---
+++ tdesign-mobile-starter/src/main.ts
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+const setRem = () => {
+    const html = document.querySelector('html')!;
+    const width = html.getBoundingClientRect().width;
+    html.style.fontSize = width / 10 + 'px';
+};
+
+setRem();
+
+window.onresize = () => {
+    setRem();
+};
+
+createApp(App).mount('#app')
---
+++ tdesign-mobile-starter/src/postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    plugins: {
+      'postcss-pxtorem': {
+        rootValue: 37.5, // 375px 设计稿下，1rem 等于 37.5px
+        propList: ['*'],
+        unitPrecision: 5, // 保留 5 位小数
+        selectorBlackList: [], // 忽略的选择器，保持为原 px 值
+        replace: true,
+        mediaQuery: false,
+        minPixelValue: 12, // 小于 12px 不转换
+      },
+    },
+  };
+  
