99b2525feed8c199f3f9f32535b39abe0ec7df41
jianyi-gronk <jianyi_gronk@163.com>
2023-07-09 21:46:56 +0800

    refactor: change husky and conflict resolution

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
 
 const setRem = () => {
-    const html = document.querySelector('html')!;
-    const width = html.getBoundingClientRect().width;
-    html.style.fontSize = width / 10 + 'px';
+  const html = document.querySelector('html')!;
+  const width = html.getBoundingClientRect().width;
+  html.style.fontSize = width / 10 + 'px';
 };
 
 setRem();
 
 window.onresize = () => {
-    setRem();
+  setRem();
 };
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
