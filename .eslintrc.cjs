f999604edd7c8d661422a492b04221e3c33d2d11
jianyi-gronk <jianyi_gronk@163.com>
2023-07-09 16:56:25 +0800

    chore: add eslint + prettier + commitlint + husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "plugin:prettier/recommended",
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:vue/vue3-essential"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "parser": "@typescript-eslint/parser",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "prettier",
+        "vue"
+    ],
+    "rules": {
+        "prettier/prettier": "error"
+    }
+}
