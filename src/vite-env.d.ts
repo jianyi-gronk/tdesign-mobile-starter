64b68df02ed3e9a3353c7a734fb300de36881940
linhuaihang <linhuaihang@163.com>
2023-07-11 16:01:02 +0800

    merge: feature/applicant_information 分支与 main 分支合并

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,9 @@
 /// <reference types="vite/client" />
-// declare module '*.vue' {
-//   import { DefineComponent } from 'vue';
-//   const component: DefineComponent<{}, {}, any>;
-//   export default component;
-// }
+declare module '*.vue' {
+  import { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
 declare module '*./router' {
   import type { DefineComponent } from 'vue-router';
   const component: DefineComponent<{}, {}, any>;
