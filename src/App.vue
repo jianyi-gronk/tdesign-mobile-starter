6f84a4bf4aca7cd389044117de2781cdf36610c3
linhuaihang <linhuaihang@163.com>
2023-07-11 14:39:53 +0800

    feat: 报名人信息页面、选择生日页面、选择职业页面开发

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue';
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- <div class="header">
+    <button @click="toPagePath('/applicant_information')">报名信息页</button>
+  </div> -->
+  <router-view></router-view>
 </template>
 
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+// import { useRouter } from 'vue-router';
+// const router = useRouter();
+// const toPagePath = (url: string) => {
+//   router.push({
+//     path: url,
+//   });
+// };
+</script>
+
 <style scoped>
-.logo {
+/* .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
@@ -26,5 +28,5 @@ import HelloWorld from './components/HelloWorld.vue';
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
-}
+} */
 </style>
