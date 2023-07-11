d82ab69bdd3f6c4a1759e4f970e78ade1bc5d9c4
jianyi-gronk <jianyi_gronk@163.com>
2023-07-11 21:23:17 +0800

    refactor: change prettier about inspect file name and relax husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,33 +1,34 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: [
     'plugin:prettier/recommended',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-essential'
   ],
   overrides: [
     {
       env: {
-        node: true,
+        node: true
       },
       files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: 'script',
-      },
-    },
+        sourceType: 'script'
+      }
+    }
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['@typescript-eslint', 'prettier', 'vue'],
   rules: {
     'prettier/prettier': 'error',
     '@typescript-eslint/no-non-null-assertion': 'off',
-  },
+    'vue/multi-word-component-names': 'off'
+  }
 };
