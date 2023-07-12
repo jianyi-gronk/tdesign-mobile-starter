b2d9513d9d244f8fc7641ae4f1c92066478279d5
Ceezyyyy <2417568802@qq.com>
2023-07-12 23:23:41 +0800

    feat: 首页重新布局

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,8 @@ import Home from './pages/home/index.vue';
 
 <style scoped lang="less">
 * {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+  //margin: 0;
+  //padding: 0;
+  //box-sizing: border-box;
 }
 </style>
