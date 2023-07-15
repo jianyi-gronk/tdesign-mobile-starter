ab7b82455b4cef2f94d2641562630e0e52a69e5a
幻小芒 <3488144878@qq.com>
2023-07-15 14:07:42 +0800

    ss

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -59,7 +59,7 @@
             </t-tab-panel>
           </t-tabs>
         </div>
-        <div class="filter" @click="visible=true">
+        <div class="filter" @click="visible = true">
           <div class="filter_">
             <icon-font name="filter" size="large" />
             <span style="font-size: 0.3733rem">筛选</span>
@@ -218,7 +218,7 @@
 <script lang="ts" setup>
 import { IconFont } from 'tdesign-icons-vue-next';
 import { ref } from 'vue';
-import moment from 'moment';
+// import moment from 'moment';
 const visible = ref<boolean>(false);
 let date = ref<string>('2023年3月10日-3月16日');
 // 区间选择日期
@@ -252,144 +252,144 @@ const onChange = (val: string) => {
 </script>
 <style>
 .热门推荐 {
-    width: 80px;
-    height: 28px;
-    opacity: 1;
-    color: #000000e6;
-    font-size: 20px;
-    font-weight: 600;
-    font-family: "PingFang SC";
-    text-align: left;
-    line-height: 28px;
+  width: 80px;
+  height: 28px;
+  opacity: 1;
+  color: #000000e6;
+  font-size: 20px;
+  font-weight: 600;
+  font-family: 'PingFang SC';
+  text-align: left;
+  line-height: 28px;
 }
 
 .header {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
 }
 
 .example-search {
-    width: 343px;
-    background-color: #fff;
-    padding: 21px 0px;
+  width: 343px;
+  background-color: #fff;
+  padding: 21px 0px;
 }
 
 .t-tabs {
-    margin-bottom: 16px;
+  margin-bottom: 16px;
 }
 
 .label-content {
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    span {
-        margin-left: 3px;
-    }
+  span {
+    margin-left: 3px;
+  }
 }
 
 .search-box {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 60px;
-    background-color: #f8f8f8;
-    padding: 5px;
-    box-sizing: border-box;
-    z-index: 1000;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 60px;
+  background-color: #f8f8f8;
+  padding: 5px;
+  box-sizing: border-box;
+  z-index: 1000;
 }
 
 .search-box input {
-    width: 100%;
-    height: 40px;
-    padding: 0 10px;
-    box-sizing: border-box;
+  width: 100%;
+  height: 40px;
+  padding: 0 10px;
+  box-sizing: border-box;
 }
 
 .container {
-    width: 100%;
-    padding-top: 80px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  width: 100%;
+  padding-top: 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 .placeholder {
-    height: 134px;
+  height: 134px;
 }
 
 .sticky {
-    position: -webkit-sticky;
-    position: sticky;
-    top: 166px;
-    height: 28px;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    /* padding-left: 16px; */
-    width: 100%;
+  position: -webkit-sticky;
+  position: sticky;
+  top: 166px;
+  height: 28px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  /* padding-left: 16px; */
+  width: 100%;
 }
 
 .sticky_ {
-    position: -webkit-sticky;
-    position: sticky;
-    top: 194px;
-    height: 48px;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    /* padding-left: 16px; */
-    width: 100%;
+  position: -webkit-sticky;
+  position: sticky;
+  top: 194px;
+  height: 48px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  /* padding-left: 16px; */
+  width: 100%;
 }
 
 .sticky-content {
-    padding-left: 16px;
-    box-sizing: border-box;
-    height: 28px;
-    opacity: 1;
-    color: #000000e6;
-    font-size: 20px;
-    font-weight: 600;
-    font-family: 'PingFang SC';
-    text-align: left;
-    line-height: 28px;
+  padding-left: 16px;
+  box-sizing: border-box;
+  height: 28px;
+  opacity: 1;
+  color: #000000e6;
+  font-size: 20px;
+  font-weight: 600;
+  font-family: 'PingFang SC';
+  text-align: left;
+  line-height: 28px;
 }
 
 .other-content {
-    width: 343px;
-    height: 120px;
-    border-radius: 9px;
-    opacity: 1;
-    background: #fff;
-    box-shadow: 0 6px 30px 5px #0000000d, 0 16px 24px 2px #0000000a,
-        0 8px 10px -5px #00000014;
-    margin-bottom: 20px;
+  width: 343px;
+  height: 120px;
+  border-radius: 9px;
+  opacity: 1;
+  background: #fff;
+  box-shadow: 0 6px 30px 5px #0000000d, 0 16px 24px 2px #0000000a,
+    0 8px 10px -5px #00000014;
+  margin-bottom: 20px;
 }
 
 .filter {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 3.3333rem;
-    height: 1.28rem;
-    position: absolute;
-    margin-left: 6.6667rem;
-    /* margin-top: 11.7067rem; */
-    margin-bottom: 10px;
-    border-left: 1px solid #e7e7e7ff;
-    border-bottom: 0.5px solid #e7e7e7ff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 3.3333rem;
+  height: 1.28rem;
+  position: absolute;
+  margin-left: 6.6667rem;
+  /* margin-top: 11.7067rem; */
+  margin-bottom: 10px;
+  border-left: 1px solid #e7e7e7ff;
+  border-bottom: 0.5px solid #e7e7e7ff;
 }
 
 .filter_ {
-    width: 3.3333rem;
-    height: 0.5867rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-left: 1px solid #e7e7e7ff;
+  width: 3.3333rem;
+  height: 0.5867rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-left: 1px solid #e7e7e7ff;
 }
 .fil_header {
   text-align: center;
@@ -409,7 +409,7 @@ const onChange = (val: string) => {
     flex-direction: column;
     margin-top: 20px;
 
-    .item_choose{
+    .item_choose {
       display: flex;
       padding: 0 10px;
       align-items: center;
@@ -432,7 +432,7 @@ const onChange = (val: string) => {
         margin-bottom: 13px;
       }
     }
-    .item_content{
+    .item_content {
       margin-bottom: 15px;
     }
   }
