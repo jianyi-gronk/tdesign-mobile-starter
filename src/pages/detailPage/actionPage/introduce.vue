b40fbbf1e7f63d1181c200ad3e7b0c8851254f47
XIE <2580924475@qq.com>
2023-07-19 09:06:59 +0800

    feat:完成详情页的基本结构

---
+++ src/pages/detailPage/actionPage/introduce.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="introduce">
+    <div class="activityIntroduce">活动介绍</div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="less">
+.introduce {
+  width: 343px;
+  height: 360px;
+  opacity: 1;
+  margin: 0 16px;
+  background-color: pink;
+  .activityIntroduce {
+    width: 343px;
+    height: 24px;
+    opacity: 1;
+    color: #000000e6;
+    font-size: 16px;
+    font-weight: 600;
+    font-family: 'PingFang SC';
+    text-align: left;
+    line-height: 24px;
+  }
+}
+</style>
